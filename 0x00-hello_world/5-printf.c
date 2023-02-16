350b35240040d412af7e248531476ffb117c8e09
Okunola Toheeb <olatundetoheeb0@gmail.comclear>
2023-02-16 14:31:56 -0800

    my c script project

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - print a line with put sfunction
+*
+* Description: using the main function
+* this program prints "Programming is like building a multilingual puzzle
+* Return: 0
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
