050aa6d4339955ae4973352a92fa1d1773a0a3a8
Okunola Toheeb <olatundetoheeb0@gmail.comclear>
2023-02-17 14:05:57 -0800

    new project

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Description: using the main function
+* this program prints "Programming is positive, zero, or negative
+* Return: 0 (success)
+*/
+int main(void)
+{
+char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+	putchar(ch);
+}
+putchar('\n');
+return (0);
+}
