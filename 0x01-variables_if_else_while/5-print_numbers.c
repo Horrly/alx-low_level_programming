2bab053cf9eb85bbd8827514bcc78564953d8866
Okunola Toheeb <olatundetoheeb0@gmail.comclear>
2023-02-17 14:40:37 -0800

    my project

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
+
 /**
- * main - Entry point 
- * Description: using the main function
- * this program prints "Programming is positive, zero, or negative
- * Return: 0( success)
+ *main - print 0123456789
+ *Return: Always 0 (Success)
  */
+
 int main(void)
 {
-int i;
-for (i = 0; i < 10; i++)
-{
-	printf("%i", i);
-}
-printf("\n");
-return (0);
+	int n = 0;
+
+	while (n < 10)
+	{
+		printf("%i", n);
+		n++;
+	}
+	putchar('\n');
+
+	return (0);
 }
