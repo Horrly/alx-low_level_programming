b4abca4a74161f8be12036686160dfda5f850803
Okunola Toheeb <olatundetoheeb0@gmail.comclear>
2023-02-17 14:38:12 -0800

    my project

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 /**
  * main - Entry point 
- *
  * Description: using the main function
  * this program prints "Programming is positive, zero, or negative
- * Return: 0 success)(
+ * Return: 0( success)
  */
 int main(void)
 {
