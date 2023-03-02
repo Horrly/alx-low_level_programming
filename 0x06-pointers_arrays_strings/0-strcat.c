a400d9af8496cc1fa9d47c27bf635f7d331979f8
Okunola Toheeb <olatundetoheeb0@gmail.comclear>
2023-03-02 15:43:06 -0800

    m

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,20 +7,18 @@
  * Return: pointer to resulting string `dest`
  */
 
-char *_strcat(char *dest, char *src)
-{
-	int i, c;
-
-	for (i = 0; dest[i] != '\0'; i++)
-		;
-
-	for (c = 0; src[c] != '\0'; c++)
-	{
-		dest[i] = src[c];
-		i++;
-	}
-
-	dest[i] = '\0';
-	return (dest);
+char* strcat(char *a, char *b) {
+    while (*a++);
+    a--;
+    while (*a++ = *b++);
+    return a;
 }
+
+char *str_a = "a string";
+char *str_b = "a long long long string";
+char *str_c = "yet another string";
+char *result;
+
+result = strcat(str_a, str_b);
+result = strcat(result, str_c);
 
